4899499a47a4ea1aec6ac16b59692a094ae12e3b
shareur <liuyang_maple@hotmail.com>
2025-09-19 18:53:47 +0300

    astro.config.mjs ;

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://imyang.world', // 你的域名import tailwind from "@astrojs/tailwind";
-export default defineConfig({ site:'https://imyang.world', base:'/master/', output:'static', integrations:[tailwind()] });
-
-  base: '/master/',             // 发布到 gh-pages/master/ 子目录
-  output: 'static'
+  site: 'https://imyang.world',
+  base: '/master/',
+  output: 'static',
 });
