1c4dd542cfb39ee3f6035fabc539913d30d5e25b
Yang <13814726+shareur@users.noreply.github.com>
2025-09-19 16:03:37 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://imyang.world', // 你的域名
+  site: 'https://imyang.world', // 你的域名import tailwind from "@astrojs/tailwind";
+export default defineConfig({ site:'https://imyang.world', base:'/master/', output:'static', integrations:[tailwind()] });
+
   base: '/master/',             // 发布到 gh-pages/master/ 子目录
   output: 'static'
 });
